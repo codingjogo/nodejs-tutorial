52750f920af597c3b87397080eae3453b8d5c576
Joshua Gonzales (codingjogo) <codingjogo@gmail.com>
2024-08-19 08:04:40 +0800

    refractor : Shorthand for setting a header and status code

--- server.js
+++ server.js
@@ -4,10 +4,13 @@ const PORT = 8000;
 const server = http.createServer((req, res) => {
     // [setHeader-method](https://www.geeksforgeeks.org/node-js-response-setheader-method/)
     // check header in developer tools on Google Chrome under the Network tab -> localhost (under name sidebar) -> click Header tab
-    res.setHeader('Content-Type', 'text/plain');
-    // Changing status code
-    res.statusCode = 404; // Means not found
-    res.end('<h1>Hello World</h1>');
+    // res.setHeader('Content-Type', 'text/plain');
+    // // Changing status code
+    // res.statusCode = 404; // Means not found
+    // res.end('<h1>Hello World</h1>');
+
+    res.writeHead(200, { 'Content-Type' : 'application/json'})
+    res.end(JSON.stringify({message: 'Server Status is "OK"'}))
 })
 
 server.listen(PORT, () => {
