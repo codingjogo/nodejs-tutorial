5ca986afdd22dffaf4b999528730eb3c7ce0885f
Joshua Gonzales (codingjogo) <codingjogo@gmail.com>
2024-08-19 19:13:08 +0800

    feat : Add package "nodemon" and GITIGNORE

--- server.js
+++ server.js
@@ -2,12 +2,7 @@ import http from 'http';
 const PORT = 8000;
 
 const server = http.createServer((req, res) => {
-    // [setHeader-method](https://www.geeksforgeeks.org/node-js-response-setheader-method/)
-    // check header in developer tools on Google Chrome under the Network tab -> localhost (under name sidebar) -> click Header tab
-    // res.setHeader('Content-Type', 'text/plain');
-    // // Changing status code
-    // res.statusCode = 404; // Means not found
-    // res.end('<h1>Hello World</h1>');
+    // npm modules: The free npm Registry has become the center of JavaScript code sharing, and with more than two million packages, the largest software registry in the world
 
     res.writeHead(200, { 'Content-Type' : 'application/json'})
     res.end(JSON.stringify({message: 'Server Status is "OK"'}))
