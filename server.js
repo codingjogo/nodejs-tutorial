7370613e2ff201f83a80f101fa4498627cfd803a
Joshua Gonzales (codingjogo) <codingjogo@gmail.com>
2024-08-19 19:39:37 +0800

    refractor : Routing

--- server.js
+++ server.js
@@ -1,24 +1,28 @@
 import http from 'http';
+import fs from 'fs/promises';
+import path from 'path';
 import url from 'url';
 const PORT = 8000;
 
 // Get current path
 const __filename = url.fileURLToPath(import.meta.url);
-const __dirname = import.meta.dirname;
+const __dirname = path.dirname(__filename);
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
     try {
         if (req.method === 'GET') {
+            let filePath;
             if (req.url === '/') {
-                res.writeHead(200, {'Content-Type' : 'text/html'});
-                res.end('<h1>Home Page</h1>');
+                filePath = path.join(__dirname, 'public', 'index.html');
             } else if (req.url === '/about') {
-                res.writeHead(200, {'Content-Type' : 'text/html'});
-                res.end('<h1>About Page</h1>');
+                filePath = path.join(__dirname, 'public', 'about.html');
             } else {
-                res.writeHead(404, {'Content-Type' : 'text/html'});
-                res.end('<h1>Not Found Page</h1>');
+                throw new Error('Not Found');
             }
+            const data = await fs.readFile(filePath);
+            res.setHeader('Content-Type', 'text/html');
+            res.write(data);
+            res.end();
         } else {
             res.writeHead(404, {'Content-Type' : 'text/html'});
             res.end('<h1>Invalid HTTP METHOD</h1>');
