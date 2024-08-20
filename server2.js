9e1ec66d72087b3ac332687813a2023790c61bc6
Joshua Gonzales (codingjogo) <codingjogo@gmail.com>
2024-08-20 10:32:53 +0800

    feat : Add middleware logger

--- server2.js
+++ server2.js
@@ -11,29 +11,36 @@ const posts = [
 	},
 ];
 
+const loggerMiddleware = (req, res, next) => {
+	console.log(`URL: ${req.url} METHOD: ${req.method}`)
+	next();
+}
+
 const server = createServer((req, res) => {
-	if (req.url === "/api/posts" && req.method === "GET") {
-		res.setHeader("Content-Type", "application/json");
-		res.write(JSON.stringify(posts));
-		res.end();
-	} else if (req.url.match(/\/api\/posts\/([0-9]+)/) && req.method === "GET") {
-		const id = req.url.split("/")[3];
-		const post = posts.find((post) => post.id === parseInt(id));
-        if (post) {
-            res.setHeader("Content-Type", "application/json");
-            res.write(JSON.stringify(post));
-            res.end();
-        }  else {
-            res.setHeader("Content-Type", "application/json");
-            res.write(JSON.stringify({message: "No user found"}));
-            res.end();
-        }
-	} else {
-		res.setHeader("Content-Type", "application/json");
-        res.statusCode = 404;
-		res.write(JSON.stringify({ message: "METHOD not VALID" }));
-		res.end();
-	}
+	loggerMiddleware(req, res, () => {
+		if (req.url === "/api/posts" && req.method === "GET") {
+			res.setHeader("Content-Type", "application/json");
+			res.write(JSON.stringify(posts));
+			res.end();
+		} else if (req.url.match(/\/api\/posts\/([0-9]+)/) && req.method === "GET") {
+			const id = req.url.split("/")[3];
+			const post = posts.find((post) => post.id === parseInt(id));
+			if (post) {
+				res.setHeader("Content-Type", "application/json");
+				res.write(JSON.stringify(post));
+				res.end();
+			}  else {
+				res.setHeader("Content-Type", "application/json");
+				res.write(JSON.stringify({message: "No user found"}));
+				res.end();
+			}
+		} else {
+			res.setHeader("Content-Type", "application/json");
+			res.statusCode = 404;
+			res.write(JSON.stringify({ message: "METHOD not VALID" }));
+			res.end();
+		}
+	})
 });
 
 server.listen(process.env.PORT, () => {
